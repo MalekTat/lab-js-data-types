074bad0d53556e8afb89a9009a1826d0a8991ff6
Malek T <malek_tatin@yahoo.com>
2024-10-23 14:00:37 +0200

    Solved lab

--- index.js
+++ index.js
@@ -9,10 +9,11 @@ const s5 = "and";
 
 // Concatenate the string variables into one new string
 
+const tongueTwister = `${s1} ${s2} ${s3} ${s4} ${s5} ${s3} ${s2} ${s1} ${s4}` ;
 
 // Print out the concatenated string
 
-
+console.log(tongueTwister)
 
 
 /*******************************************
@@ -23,10 +24,14 @@ const part2 = "script";
 
 // Convert the last letter of part1 and part2 to uppercase and concatenate the strings
 
+const result1 = `${part1.slice(0,part1.length-1)}${part1[part1.length-1].toLocaleUpperCase()}`
+const result2 = `${part2.slice(0,part2.length-1)}${part2[part2.length-1].toLocaleUpperCase()}`
+const result = result1 + result2
+
 
 // Print the cameLtaiL-formatted string
 
-
+console.log(result)
 
 
 /*******************************************
@@ -35,10 +40,10 @@ const part2 = "script";
 const billTotal = 84;
 
 // Calculate the tip (15% of the bill total)
-
+const tipAmount = 84 * 15 / 100
 
 // Print out the tipAmount
-
+console.log(tipAmount)
 
 
 
@@ -48,10 +53,11 @@ const billTotal = 84;
 
 // Generate a random integer between 1 and 10 (inclusive)
 
+const randomNumber = Math.ceil( Math.random() * 10 )
 
 // Print the generated random number
 
-
+console.log(randomNumber)
 
 /*******************************************
     Iteration 3.1 | Booleans
@@ -74,3 +80,11 @@ const expression5 = !a || !b;
 const expression6 = !(a || b);
 
 const expression7 = a && a;
+
+console.log('expression1 :',expression1)
+console.log('expression2 :',expression2)
+console.log('expression3 :',expression3)
+console.log('expression4 :',expression4)
+console.log('expression5 :',expression5)
+console.log('expression6 :',expression6)
+console.log('expression7 :',expression7)
